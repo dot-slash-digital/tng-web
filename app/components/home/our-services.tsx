1f3abea9d0e1734b6bb26281ecc438e41b396d52
PixelPirate <andres.f.esca98@gmail.com>
2023-06-12 21:02:05 -0500

    fonts added and styles placed in the proper file

--- app/components/home/our-services.tsx
+++ app/components/home/our-services.tsx
@@ -5,35 +5,25 @@ import SectionContainer from "@components/section-container";
 import styles from "@styles/home/our-services.module.scss";
 
 export default () => {
-  const rectangleStyle = {
-    width: '639px',
-    height: '240px',
-    marginRight: '48px',
-    background: '#e0e0e0',
-    
-  };
-  const contentStyle = {
-    justifyContent: 'left',
-    marginRight: '21px', // Add margin to the left
-  }; 
+
   return (
     <div className={styles.component}>
         <SectionContainer>
           <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', flexDirection: 'column' }}>
-            <h1>Our Services</h1>
+            <div className={styles.title}>Our Services</div>
             <div style={{ display: 'flex' }}>
               <div style={{ display: 'flex', alignItems: 'left', flexDirection: 'column' }}>
-                <div style={rectangleStyle}></div>
-                <div style={contentStyle}>
-                  <h2 className="Mulish">Home Remodeling</h2>
-                  <p>Service description goes here Service description goes here Service description goes here Service description goes here Service description goes here Service description goes here</p>
+                <div className={styles.rectangle}></div>
+                <div className={styles.content}>
+                  <div className={styles.subtitle}>Home Remodeling</div>
+                  <p className={styles.paragraph}>Service description goes here Service description goes here Service description goes here Service description goes here Service description goes here Service description goes here</p>
                 </div>
 
               </div>
-              <div style={contentStyle}>
-                <div style={rectangleStyle}></div>
-                <h2>New Home Construction</h2>
-                <p>Service description goes here Service description goes here Service description goes here Service description goes here</p>
+              <div className={styles.content}>
+                <div className={styles.rectangle}></div>
+                <div className={styles.subtitle}>New Home Construction</div>
+                <p className={styles.paragraph}>Service description goes here Service description goes here Service description goes here Service description goes here</p>
 
               </div>
             </div>
