69ca70605cc48dd06ee1a145065d1feca2b2295e
PixelPirate <andres.f.esca98@gmail.com>
2023-07-09 21:42:53 -0500

    second commit

--- app/components/home/our-process.tsx
+++ app/components/home/our-process.tsx
@@ -1,3 +1,4 @@
+import button from "@components/button";
 import SectionContainer from "@components/section-container";
 import content from "@content";
 
@@ -6,9 +7,80 @@ import styles from "@styles/home/our-process.module.scss";
 export default () => {
   const { list, title } = content.home.ourProcess;
 
+  
+
   return (
     <div className={styles.component}>
-      <SectionContainer>Our process</SectionContainer>
+      <SectionContainer>
+        <div className={styles.title}>{title}</div>
+        <div className={styles.container}>
+          <div className={styles["sub-container-left"]}>
+            {list.map((step, index) => (
+              <div key={index}>
+                {(index === 0 || index ===2) && (
+                <div className={styles.rectangle}>
+                  <div className={styles.circle}>
+                    <div className={styles["circle-num"]}>{index + 1}</div>  
+                  </div>
+                  <div className={styles.square}/>
+                  <div className={styles["text-box"]}>
+                    <div className={styles["sub-title"]}>
+                    {step.title}
+                    </div>
+                    {step.description.map((text, index) => (
+                    <div className={styles.description}>
+                      {text}
+                    </div>
+                    ))}
+                    {step.button && 
+                      <div className={styles.button}> 
+                        <a className={styles["button-text"]} href={step.button.destination} >
+                        {step.button.label}
+                        </a>
+                        <div className={styles.icon}/> 
+                      </div>
+                    }
+                  </div>
+                </div>
+                )}
+              </div>
+            ))}
+          </div>
+          <div className={styles["sub-container-right"]}>
+          {list.map((step, index) => (
+              <div key={index}>
+                {(index === 1 || index ===3) && (
+                <div className={styles.rectangle}>
+                  <div className={styles.circle}>
+                    <div className={styles["circle-num"]}>{index + 1}</div> 
+                  </div>
+                  <div className={styles.square}/>
+                  <div className={styles["text-box"]}>
+                    <div className={styles["sub-title"]}>
+                    {step.title}
+                    </div>
+                    {step.description.map((text, index) => (
+                    <div className={styles.description}>
+                      {text}
+                    </div>
+                    ))}
+                    {step.button && 
+                      <div className={styles.button}> 
+                      <a className={styles["button-text"]} href={step.button.destination} >
+                      {step.button.label}
+                      </a>
+                      <div className={styles.icon}/> 
+                    </div>                    
+                    }
+                  </div>
+                </div>
+                )}
+              </div>
+            ))}
+          </div>
+        </div>
+      </SectionContainer>
     </div>
   );
 };
+
