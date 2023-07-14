f1d696f4e3b1d4e24eeb9604bc852e044a060391
PixelPirate <andres.f.esca98@gmail.com>
2023-07-14 17:24:30 -0500

    .

--- app/components/home/meet-the-team.tsx
+++ app/components/home/meet-the-team.tsx
@@ -12,20 +12,20 @@ export default () => {
         <div className={styles.container}>
           <div className={styles.title}>{title}</div>
           <div className={styles["text-box"]}>
-              {teamMembers.map((member, index) => (
-                <div key={index}>
-                  <div className={styles["tm-box"]}>
-                    <div className={styles["tm-image"]}></div>
-                    <div className={styles["team-member"]}>{member.title}</div>
-                    <div className={styles["tm-description"]}>{member.description}</div>
+            {teamMembers.map((member, index) => (
+              <div key={index}>
+                <div className={styles["tm-box"]}>
+                  <div className={styles["tm-image"]}></div>
+                  <div className={styles["team-member"]}>{member.title}</div>
+                  <div className={styles["tm-description"]}>
+                    {member.description}
                   </div>
                 </div>
-              ))}             
+              </div>
+            ))}
           </div>
         </div>
       </SectionContainer>
     </div>
   );
 };
-
-
