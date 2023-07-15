e622877bcdc76efc1ca09be73043eb78835b85a0
Matthew Bierman <matthew.bierman2@gmail.com>
2023-07-15 01:26:44 -0400

    Code fixes

--- app/components/home/our-process.tsx
+++ app/components/home/our-process.tsx
@@ -1,75 +1,68 @@
+import Button, { ButtonType } from "@components/button";
 import SectionContainer from "@components/section-container";
 import content from "@content";
+import { ProcessItem } from "@types";
 
 import styles from "@styles/home/our-process.module.scss";
+
+const Card = ({
+  button,
+  description,
+  icon,
+  index,
+  title,
+}: ProcessItem & { index: number }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.circle}>
+        <div className={styles.number}>{index + 1}</div>
+      </div>
+      <div
+        className={styles.icon}
+        style={{ backgroundColor: `url(${icon})` }}
+      />
+      <div>
+        <div className={styles["card-title"]}>{title}</div>
+        <div className={styles["card-description"]}>
+          {description.map((paragraph, index) => (
+            <div className={styles["card-paragraph"]} key={index}>
+              {paragraph}
+            </div>
+          ))}
+        </div>
+        {button && (
+          <div className={styles["card-button"]}>
+            <Button {...button} type={ButtonType.OUTLINE} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default () => {
   const { list, title } = content.home.ourProcess;
 
+  const cards = [
+    list
+      .map((card, index) => ({ ...card, index }))
+      .filter(({ index }) => index % 2 === 0),
+    list
+      .map((card, index) => ({ ...card, index }))
+      .filter(({ index }) => index % 2 !== 0),
+  ];
+
   return (
     <div className={styles.component}>
       <SectionContainer>
-        <div className={styles.title}>{title}</div>
-        <div className={styles.container}>
-          <div className={styles["sub-container-left"]}>
-            {list.map((step, index) => (
-              <div key={index}>
-                {index % 2 === 0 && (
-                  <div className={styles.rectangle}>
-                    <div className={styles.circle}>
-                      <div className={styles["circle-num"]}>{index + 1}</div>
-                    </div>
-                    <div className={styles.square} />
-                    <div className={styles["text-box"]}>
-                      <div className={styles["sub-title"]}>{step.title}</div>
-                      {step.description.map((text, index) => (
-                        <div className={styles.description}>{text}</div>
-                      ))}
-                      {step.button && (
-                        <div className={styles.button}>
-                          <a
-                            className={styles["button-text"]}
-                            href={step.button.destination}
-                          >
-                            {step.button.label}
-                          </a>
-                          <div className={styles.icon} />
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                )}
-              </div>
-            ))}
-          </div>
-          <div className={styles["sub-container-right"]}>
-            {list.map((step, index) => (
-              <div key={index}>
-                {index % 2 !== 0 && (
-                  <div className={styles.rectangle}>
-                    <div className={styles.circle}>
-                      <div className={styles["circle-num"]}>{index + 1}</div>
-                    </div>
-                    <div className={styles.square} />
-                    <div className={styles["text-box"]}>
-                      <div className={styles["sub-title"]}>{step.title}</div>
-                      {step.description.map((text, index) => (
-                        <div className={styles.description}>{text}</div>
-                      ))}
-                      {step.button && (
-                        <div className={styles.button}>
-                          <a
-                            className={styles["button-text"]}
-                            href={step.button.destination}
-                          >
-                            {step.button.label}
-                            <div className={styles.icon} />
-                          </a>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                )}
+        <div className={styles.content}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.columns}>
+            {cards.map((column, columnIndex) => (
+              <div className={styles.column} key={columnIndex}>
+                {column.map((card) => (
+                  <Card {...card} key={card.index} />
+                ))}
               </div>
             ))}
           </div>
