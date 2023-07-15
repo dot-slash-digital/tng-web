128d61b57c8d818cebbbeae434f2f66d758fe1c8
Dev-felipe22 <108008803+Dev-felipe22@users.noreply.github.com>
2023-07-15 00:27:05 -0500

    [Home Page - Process] Desktop breakpoint (#6)
    * first commit
    
    * second commit
    
    * .
    
    * ..
    
    * run Prettier
    
    * .
    
    * Prettier
    
    * .
    
    * done
    
    * Code fixes
    
    ---------
    
    Co-authored-by: Matthew Bierman <matthew.bierman2@gmail.com>

--- app/components/home/our-process.tsx
+++ app/components/home/our-process.tsx
@@ -1,14 +1,73 @@
+import Button, { ButtonType } from "@components/button";
 import SectionContainer from "@components/section-container";
 import content from "@content";
+import { ProcessItem } from "@types";
 
 import styles from "@styles/home/our-process.module.scss";
+
+const Card = ({
+  button,
+  description,
+  icon,
+  index,
+  title,
+}: ProcessItem & { index: number }) => {
+  return (
+    <div className={styles.card}>
+      <div className={styles.circle}>
+        <div className={styles.number}>{index + 1}</div>
+      </div>
+      <div
+        className={styles.icon}
+        style={{ backgroundColor: `url(${icon})` }}
+      />
+      <div>
+        <div className={styles["card-title"]}>{title}</div>
+        <div className={styles["card-description"]}>
+          {description.map((paragraph, index) => (
+            <div className={styles["card-paragraph"]} key={index}>
+              {paragraph}
+            </div>
+          ))}
+        </div>
+        {button && (
+          <div className={styles["card-button"]}>
+            <Button {...button} type={ButtonType.OUTLINE} />
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default () => {
   const { list, title } = content.home.ourProcess;
 
+  const cards = [
+    list
+      .map((card, index) => ({ ...card, index }))
+      .filter(({ index }) => index % 2 === 0),
+    list
+      .map((card, index) => ({ ...card, index }))
+      .filter(({ index }) => index % 2 !== 0),
+  ];
+
   return (
     <div className={styles.component}>
-      <SectionContainer>Our process</SectionContainer>
+      <SectionContainer>
+        <div className={styles.content}>
+          <div className={styles.title}>{title}</div>
+          <div className={styles.columns}>
+            {cards.map((column, columnIndex) => (
+              <div className={styles.column} key={columnIndex}>
+                {column.map((card) => (
+                  <Card {...card} key={card.index} />
+                ))}
+              </div>
+            ))}
+          </div>
+        </div>
+      </SectionContainer>
     </div>
   );
 };
