b31685de63215d19d853b9b059038781af8ac7bb
Matthew Bierman <matthew.bierman2@gmail.com>
2023-08-09 00:59:34 -0400

    [Gallery - Previous Clients] Lightbox for images

--- app/components/gallery/previous-clients.tsx
+++ app/components/gallery/previous-clients.tsx
@@ -1,10 +1,34 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { createPortal } from "react-dom";
+
 import SectionContainer from "@components/section-container";
 import content from "@content";
 
 import styles from "@styles/gallery/previous-clients.module.scss";
 
+const Lightbox = ({ close, src }: { close: () => void; src: string }) => {
+  return (
+    <div className={styles["lightbox-background"]} onClick={close}>
+      <img
+        {...{ src }}
+        className={styles["lightbox-image"]}
+        alt="Gallery image"
+      />
+    </div>
+  );
+};
+
 export default () => {
   const { grid, title } = content.gallery.previousClients;
+
+  const [openedImage, setOpenedImage] = useState<number | undefined>();
+
+  useEffect(() => {
+    document.body.style.overflow =
+      openedImage !== undefined ? "hidden" : "initial";
+  }, [openedImage]);
 
   return (
     <div className={styles.component}>
@@ -16,12 +40,21 @@ export default () => {
               <div
                 className={styles.image}
                 key={index}
+                onClick={() => setOpenedImage(index)}
                 style={{ backgroundImage: `url(${image})` }}
               />
             ))}
           </div>
         </div>
       </SectionContainer>
+      {openedImage !== undefined &&
+        createPortal(
+          <Lightbox
+            close={() => setOpenedImage(undefined)}
+            src={grid[openedImage]}
+          />,
+          document.body
+        )}
     </div>
   );
 };
